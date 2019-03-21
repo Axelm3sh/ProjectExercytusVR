060a8868db5e4a1f68f2c335afbe4ec17f59cc4c
Axelm3sh <axel.m3sh@gmail.com>
2019-03-20 18:38:26 -0700

    Testing JS script
    Testing js script that removes inner content to replace with a "WIP" text

---
+++ testOP.js
@@ -0,0 +1,14 @@
+var indexPageContent = document.querySelector("#Content1");
+
+//document.onload = function()
+window.onload = function()
+{
+    
+//    indexPageContent.remove();
+    while (indexPageContent.firstChild) 
+    {
+        indexPageContent.removeChild(indexPageContent.firstChild);
+    }
+    
+    indexPageContent.innerHTML = "<p>Website is under development! Please check back later!</p>";
+};
